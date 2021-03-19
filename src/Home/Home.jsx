fc18e4122e8fd41a4c80a0a03f59cae4b696f526
Samson Akanbi <samson.akanbi@outlook.com>
2021-03-19 07:39:03 +0100

    Worked with click events

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
 const Home = () => {
+    const handleClick = (e) => {
+        console.log('Hello Ninjas', e);
+    }
+
+    const handleClickWithParenthesis = (name, e) => {
+        console.log('hello ' + name, e.target);
+    }
+
     return (
         <div className="home">
             <h2>Home Page</h2>
+            <button onClick={handleClick}>Click Me</button>
+            <button onClick={(e) => handleClickWithParenthesis('mario', e)}>Click Me</button>
         </div>
     )
 }
