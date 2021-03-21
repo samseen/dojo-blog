c9dc1bc98efc96248fa74b8f21eb2e7fe1569881
Samson Akanbi <samson.akanbi@outlook.com>
2021-03-21 15:31:48 +0100

    Tried out useEffect.

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import BlogList from '../BlogList';
 
 const Home = () => {
@@ -12,6 +12,11 @@ const Home = () => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs)
     }
+
+    useEffect(() => {
+        console.log("Use Effect Ran");
+        console.log(blogs);
+    });
 
     return (
         <div className="home">
