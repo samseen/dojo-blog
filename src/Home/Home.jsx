45f5f90724e3d334a2bfafbf275b2dfa12854380
Samson Akanbi <samson.akanbi@outlook.com>
2021-03-21 15:04:09 +0100

    Filtered items and passed it in props.

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -5,12 +5,13 @@ const Home = () => {
     const [blogs, setBlogs] = useState([
         { title: 'My new website', body: 'lorem ipsum...', author: 'Mario', id: 1 },
         { title: 'Welcome Party', body: 'lorem ipsum...', author: 'Yoshi', id: 2 },
-        { title: 'Web Dev top tips', body: 'lorem ipsum...', author: 'Luigi', id: 3 }
+        { title: 'Web Dev top tips', body: 'lorem ipsum...', author: 'Mario', id: 3 }
     ]);
 
     return (
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs!" />
+            <BlogList blogs={blogs.filter((blog) => blog.author === 'Mario')} title="Mario's Blogs!" />
         </div>
     )
 }
