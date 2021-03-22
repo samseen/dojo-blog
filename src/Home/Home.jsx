438b33f937fd643c0802527bdcd4d4ea7c7cee24
Samson Akanbi <samson.akanbi@outlook.com>
2021-03-22 13:26:20 +0100

    Conditional Loading Message.

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -3,11 +3,7 @@ import BlogList from '../BlogList';
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
-
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs)
-    }
+    const [isPending, setIsPending] = useState(true);
 
     useEffect(() => {
         fetch('http://localhost:8000/blogs')
@@ -15,14 +11,15 @@ const Home = () => {
                 return res.json();
             })
             .then(data => {
-                console.log(data);
                 setBlogs(data);
+                setIsPending(false);
             })
     }, []);
 
     return (
         <div className="home">
-            {blogs && <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />}
+            { isPending && <div>Loading...</div> }
+            {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
         </div>
     )
 }
